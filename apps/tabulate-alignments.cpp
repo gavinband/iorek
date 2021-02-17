9382aa8fc09f2f81376c7cc0cce6cb99bd72690b
gav <>
2021-02-17 23:30:02 +0000

    tabulate-alignments: get package revision right
    FossilOrigin-Name: 264c60f1272a2847626442507d07fd173d9b80b430196d5993fd3d72f55dfed9

--- apps/tabulate-alignments.cpp
+++ apps/tabulate-alignments.cpp
@@ -36,7 +36,7 @@ namespace seqlib = SeqLib;
 
 namespace globals {
 	std::string const program_name = "tabulate-alignments" ;
-	std::string const program_version = svelte_version ;
+	std::string const program_version = package_version ;
 	std::string const program_revision = std::string( package_revision ).substr( 0, 7 ) ;
 }
 
