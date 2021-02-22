4d6982708a09542dd5236c258f1cc23f48e07d34
gav <>
2021-02-22 13:57:18 +0000

    KmerHashIterator: match jellyfish encoding
    FossilOrigin-Name: a6c781d6cadc4a8b8b2e1e929ea414d59a866faeb3ee159c832199c3a831e3ba

--- genfile/include/genfile/kmer/KmerHashIterator.hpp
+++ genfile/include/genfile/kmer/KmerHashIterator.hpp
@@ -13,10 +13,13 @@
 
 namespace genfile {
 	namespace kmer {
+		// Decode hash into a sequence.
+		// This encoding matches that of jellyfish
 		std::string decode_hash( uint64_t hash, std::size_t k ) {
 			std::string result( k, ' ' ) ;
+
 			for( std::size_t i = 0; i < k; ++i ) {
-				switch( (hash >> (2*i)) & 0x3 ) {
+				switch( (hash >> (2*(k-i-1))) & 0x3 ) {
 					case 0:
 						result[i] = 'A' ;
 						break ;
@@ -24,16 +27,24 @@ namespace genfile {
 						result[i] = 'T' ;
 						break ;
 					case 1:
-						result[i] = 'G' ;
+						result[i] = 'C' ;
 						break ;
 					case 2:
-						result[i] = 'C' ;
+						result[i] = 'G' ;
 						break ;
 				}
 			}
 			return result ;
 		}
 
+		// KmerHashIterator iterates over all kmers in a sequence of A, C, G and T.
+		// It encodes each kmer as a hash value inside a 64-bit integer
+		// where A->00, C->01, G->10 and T->11
+		// A total of 2k bits of the hash are used.
+		// Earlier bases are encoded in higher-order bits.
+		// The hash of the kmer and its reverse complement are simultaneously computed.
+		// Important: the hash values computed by this class are compatible with those of the mer_dna
+		// class used by the Jellyfish package.
 		template< typename Iterator >
 		struct KmerHashIterator {
 			KmerHashIterator( Iterator begin, Iterator const end, std::size_t k ):
@@ -47,31 +58,31 @@ namespace genfile {
 				m_mask( 0xFFFFFFFFFFFFFFFF << (64 - m_k*2))
 			{
 				assert( m_k < 33 ) ;
-
 				// fill hashes with first k bases
 				for( m_cursor = 0; m_cursor < m_k && m_begin != m_end; ++m_begin, ++m_cursor ) {
-					m_reverse_complement_hash <<= 2 ;
+					m_forward_hash <<= 2 ;
+					m_reverse_complement_hash >>= 2 ;
 					++m_last_n_base ;
 
 					uint8_t const encoded = forward_encode_base( *m_begin ) ;
 					m_last_n_base = (encoded < 4) ? m_last_n_base : 0 ;
-					m_forward_hash |= uint64_t(encoded & 0x3) << (m_cursor*2) ;
-					m_reverse_complement_hash |= ((
+					m_forward_hash |= uint64_t(encoded & 0x3) ;
+					m_reverse_complement_hash |= uint64_t(
 						(encoded < 4) ? reverse_complement( encoded ) : 0
-					)) ;
+					) << (2*m_k-2) ;
 				}
 			}
 
 			KmerHashIterator& operator++() {
 				// Update forward and reverse complement hashes with next base.
-				m_forward_hash >>= 2 ;
-				m_reverse_complement_hash <<= 2 ;
+				m_forward_hash <<= 2 ;
+				m_reverse_complement_hash >>= 2 ;
 				m_last_n_base += (m_last_n_base < m_k) ? 1 : 0 ;
 
 				uint8_t const encoded = forward_encode_base( *m_begin ) ;
 				m_last_n_base = (encoded < 4) ? m_last_n_base : 0 ;
-				m_forward_hash |= ((encoded & 0x3) << (m_k*2-2));
-				m_reverse_complement_hash |= ((encoded < 4) ? reverse_complement( encoded ) : 0) ;
+				m_forward_hash |= (encoded & 0x3) ;
+				m_reverse_complement_hash |= ((encoded < 4) ? reverse_complement( encoded ) : 0) << (m_k*2-2) ;
 
 				++m_begin ;
 				return *this ;
@@ -109,17 +120,18 @@ namespace genfile {
 			uint8_t forward_encode_base( char const base ) const {
 				switch( base ) {
 					// Important: complementary bases are bit-inverses of each other.
+					// Important: this encoding matches that of jellyfish.
 					case 'a':
 					case 'A':
 						return 0u ; // 00
 					case 't':
 					case 'T':
 						return 3u ; // 11
-					case 'g':
-					case 'G':
-						return 1u ; // 01
 					case 'c':
 					case 'C':
+						return 1u ; // 01
+					case 'g':
+					case 'G':
 						return 2u ; // 10
 					default:
 						// We use 4 to denote an unrecognised base.
