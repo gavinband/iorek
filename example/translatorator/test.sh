d4e13927ca97664b0e9001b648e4c7f86a54bc30
Gavin Band <gavin.band@well.ox.ac.uk>
2026-05-18 17:43:18 +0100

    Fix translatorator test scripts

--- example/translatorator/test.sh
+++ example/translatorator/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-../../build/apps/translatorator -sequences reads.bam -cds-kmers GTTAA AAAAA -range chr1:1-60
-../../build/apps/translatorator -sequences reads.bam -cds-kmers TTTTT TTAAC -range chr1:1-60
-../../build/apps/translatorator -sequences reads.fasta -cds-kmers GTTAA AAAAA -range chr1:1-60
-../../build/apps/translatorator -sequences reads.fasta -cds-kmers TTTTT TTAAC -range chr1:1-60
-../../build/apps/translatorator_v0.9.1 -cluster -sequences reads.bam -cds-kmers GTTAA AAAAA -range chr1:1-60 -summary - -min-fraction-per-sample 0 -min-obs-per-sample 1
+../../build/apps/translatorator_v0.9.2 -sequences reads.bam -cds-kmers GTTAA AAAAA -range chr1:1-60
+../../build/apps/translatorator_v0.9.2 -sequences reads.bam -cds-kmers TTTTT TTAAC -range chr1:1-60
+../../build/apps/translatorator_v0.9.2 -sequences reads.fasta -cds-kmers GTTAA AAAAA -range chr1:1-60
+../../build/apps/translatorator_v0.9.2 -sequences reads.fasta -cds-kmers TTTTT TTAAC -range chr1:1-60
+../../build/apps/translatorator_v0.9.2 -cluster -sequences reads.bam -cds-kmers GTTAA AAAAA -range chr1:1-60 -summary - -min-fraction-per-sample 0 -min-obs-per-sample 1
